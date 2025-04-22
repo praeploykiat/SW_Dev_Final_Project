fdbc71c3391943659b3419051e6f164a5f8347d8
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-04-22 22:46:49 +0700

    Clean Up routes/passwordRest.js

--- routes/passwordReset.js
+++ routes/passwordReset.js
@@ -1,15 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { requestPasswordReset, resetPassword } = require('../controller/passwordReset.js');
-
-//const {getBookings,getBooking,addBooking,updateBooking,deleteBooking} = require('../controller/bookings');
-//const { protect } = require('../middleware/auth');
+const { requestPasswordReset, resetPassword } = require('../controller/passwordReset');
 
 // Route to request password reset
 router.post('/requestPasswordReset', requestPasswordReset);
 
-// Route to reset password
-// router.post('/requestPassword', protect, resetPassword);
+// Route to reset password using token and user ID
 router.put('/resetPassword/:id/:token', resetPassword);
 
 module.exports = router;
