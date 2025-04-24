018f19fdad537838020fb898a127a473d4be072f
Praeploy Kiatsuksri <praeploy05@gmail.com>
2025-04-24 21:54:10 +0700

    Comment Check Bookings on Same Date in controller/bookings

--- controller/bookings.js
+++ controller/bookings.js
@@ -93,18 +93,18 @@ exports.addBooking = async (req,res,next) => {
         if(existedBooking.length>=3&&req.user.role !== 'admin'){
             return res.status(400).json({success:false,msg:`The user with ID ${req.user.id} has already made 3 bookings`});
         }
-        //Check if the same date is already booked for this company
-        const existingAppointment = await Booking.findOne({
-            company: req.params.companyId,
-            apptDate: new Date(req.body.apptDate)
-        });
+        // //Check if the same date is already booked for this company
+        // const existingAppointment = await Booking.findOne({
+        //     company: req.params.companyId,
+        //     apptDate: new Date(req.body.apptDate)
+        // });
         
-        if(existingAppointment) {
-            return res.status(400).json({
-                success: false,
-                msg: `A Booking for ${company.name} on this date already exists`
-            });
-        }
+        // if(existingAppointment) {
+        //     return res.status(400).json({
+        //         success: false,
+        //         msg: `A Booking for ${company.name} on this date already exists`
+        //     });
+        // }
         const booking = await Booking.create(req.body);
 
         res.status(200).json({success:true,data:booking});
